37d7479db345f003ea01c016a5c135800a9cd16b
danicex <dantegaefe@gmail.com>
2025-02-09 22:17:44 +0100

    configure taiwind well

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-module.exports = {
-  content: [
-    './src/**/*.{html,js,jsx,ts,tsx}', 
-    './public/index.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
+export default {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
